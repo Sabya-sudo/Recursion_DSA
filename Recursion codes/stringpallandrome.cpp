391f2aa928afa35d4b1857bf429bd71e0a8cee24
sabya721 <sabyasacheethakurmail@gmail.com>
2024-10-13 18:31:34 +0530

    Second file

---
+++ Recursion codes/stringpallandrome.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <string>
+ using namespace std;
+bool f(int i, string &S){
+if(i>=S.size()/2)return true;
+if(S[i]!=S[S.size()-i-1])return false;
+return f(i+1,S);
+}
+using namespace std;
+int main(){ 
+string s ="madam";
+cout << f(0, s);
+return 0;
+}
